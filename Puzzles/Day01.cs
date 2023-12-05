4c33d7d1a400f19eed1d9ba86796970e3861bd93
László Zabb <laszlo.zabb@realeyesit.com>
2023-12-06 00:12:08 +0100

    Day1 (1,2)

--- Puzzles/Day01.cs
+++ Puzzles/Day01.cs
@@ -1,14 +1,58 @@
-﻿namespace Puzzles;
+﻿using System.Threading;
+using static System.Char;
+
+namespace Puzzles;
 
 public class Day01(string input) : DailyPuzzleBase(input)
 {
     public override string GetPuzzleOneSolution()
     {
-        return "";
+        return Input
+            .Split(Environment.NewLine)
+            .Sum(l => int.Parse(l.ToCharArray().First(IsDigit).ToString()) * 10 + int.Parse(l.ToCharArray().Last(IsDigit).ToString()))
+            .ToString();
     }
 
     public override string GetPuzzleTwoSolution()
     {
-        return "";
+        var digits = new Dictionary<string, int>
+        {
+            { "one", 1 },
+            { "two", 2 },
+            { "three", 3 },
+            { "four", 4 },
+            { "five", 5 },
+            { "six", 6 },
+            { "seven", 7 },
+            { "eight", 8 },
+            { "nine", 9 }
+        };
+
+        for (int i = 0; i < 10; i++)
+            digits.Add(i.ToString(), i);
+
+        var lines = Input
+            .Split(Environment.NewLine);
+
+        int sum = 0;
+        foreach (var line in lines)
+        {
+            var firstIndexOfDigits = digits
+                    .Select(d => new { Index = line.IndexOf(d.Key, StringComparison.Ordinal), Number = d.Value })
+                    .Where(dwp => dwp.Index > -1)
+                    .OrderBy(dwp => dwp.Index)
+                    .First();
+            
+            var lastIndexOfDigits = digits
+                    .Select(d => new { Index = line.LastIndexOf(d.Key, StringComparison.Ordinal), Number = d.Value })
+                    .Where(dwp => dwp.Index > -1)
+                    .OrderBy(dwp => dwp.Index)
+                    .Last();
+            
+            var number = firstIndexOfDigits.Number * 10 + lastIndexOfDigits.Number;
+            sum += number;
+        }
+
+        return sum.ToString();
     }
 }
--- Tests/Day01Tests.cs
+++ Tests/Day01Tests.cs
@@ -1,24 +1,24 @@
-using System.Runtime.CompilerServices;
 using Puzzles;
+using static NUnit.Framework.Assert;
 
 namespace Tests
 {
     [TestFixture]
     public class Day01Tests: DayTestBase<Day01>
     {
-        [TestCase(@"1",@"2")]
-        public void PuzzleOneTest(string input, string output)
-        {
-            var puzzle = GetPuzzle(input);
-            puzzle.GetPuzzleOneSolution();
-            puzzle.GetPuzzleOneSolution();
-            Assert.Pass();
-        }
+        [TestCase(@"1abc2
+pqr3stu8vwx
+a1b2c3d4e5f
+treb7uchet", "142")]
+        public void PuzzleOneTest(string input, string output) => That(GetPuzzle(input).GetPuzzleOneSolution(), Is.EqualTo(output));
 
-        [TestCase(@"1", @"2")]
-        public void PuzzleTwoTest(string input, string output)
-        {
-            Assert.Pass();
-        }
+        [TestCase(@"two1nine
+eightwothree
+abcone2threexyz
+xtwone3four
+4nineeightseven2
+zoneight234
+7pqrstsixteen", "281")]
+        public void PuzzleTwoTest(string input, string output) => That(GetPuzzle(input).GetPuzzleTwoSolution(), Is.EqualTo(output));
     }
 }
