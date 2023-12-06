ce8e93c3c268f77ec5f9d4fd4828026b18e12590
László Zabb <laszlo.zabb@realeyesit.com>
2023-12-06 02:43:05 +0100

    Day3 (2)

--- Puzzles/Day03.cs
+++ Puzzles/Day03.cs
@@ -10,13 +10,13 @@ public class Day03(string input) : DailyPuzzleBase(input)
         var h = m.Length;
         var w = m[0].Length;
 
-        bool IsAdjacentSymbol(int r, int c)
+        bool IsSymbol(int r, int c)
         {
             if (r < 0 || r >= h || c < 0 || c >= w)
                 return false;
 
-            return IsSymbol(m[r][c]);
-        };
+            return !IsDigit(m[r][c]) && m[r][c] != '.';
+        }
 
         var sum = 0;
         for (int row = 0; row < h; row++)
@@ -30,14 +30,14 @@ public class Day03(string input) : DailyPuzzleBase(input)
                     numberString += m[row][col];
                     if (!isAdjacent)
                     {
-                        isAdjacent = IsAdjacentSymbol(row, col + 1) ||
-                                     IsAdjacentSymbol(row, col - 1) ||
-                                     IsAdjacentSymbol(row + 1, col) ||
-                                     IsAdjacentSymbol(row - 1, col) ||
-                                     IsAdjacentSymbol(row + 1, col + 1) ||
-                                     IsAdjacentSymbol(row - 1, col - 1) ||
-                                     IsAdjacentSymbol(row + 1, col - 1) ||
-                                     IsAdjacentSymbol(row - 1, col + 1)
+                        isAdjacent = IsSymbol(row, col + 1) ||
+                                     IsSymbol(row, col - 1) ||
+                                     IsSymbol(row + 1, col) ||
+                                     IsSymbol(row - 1, col) ||
+                                     IsSymbol(row + 1, col + 1) ||
+                                     IsSymbol(row - 1, col - 1) ||
+                                     IsSymbol(row + 1, col - 1) ||
+                                     IsSymbol(row - 1, col + 1)
                             ;
                     }
                 }
@@ -58,11 +58,103 @@ public class Day03(string input) : DailyPuzzleBase(input)
         return sum.ToString();
     }
 
-
-    private bool IsSymbol(char ch) => !IsDigit(ch) && ch != '.';
-
     public override string GetPuzzleTwoSolution()
     {
-        return "";
+        var m = Input.Split(Environment.NewLine).ToArray();
+        var h = m.Length;
+        var w = m[0].Length;
+
+        bool IsAsterisk(int r, int c)
+        {
+            if (r < 0 || r >= h || c < 0 || c >= w)
+                return false;
+
+            return m[r][c] == '*';
+        }
+
+        var asteriskAdjacentNumbers = new List<int>[h, w];
+        for (int i = 0; i < h; i++)
+        {
+            for (int j = 0; j < w; j++)
+            {
+                asteriskAdjacentNumbers[i, j] = new List<int>();
+            }
+        }
+
+        void RegisterPossibleGears(int numberStringColToCheck, int numberStringRowToCheck, string numberStringToCheck)
+        {
+            for (int c = numberStringColToCheck - 1; c < numberStringColToCheck + numberStringToCheck.Length + 1; c++)
+            {
+                if (IsAsterisk(numberStringRowToCheck - 1, c))
+                    asteriskAdjacentNumbers[numberStringRowToCheck - 1, c] =
+                        asteriskAdjacentNumbers[numberStringRowToCheck - 1, c]
+                            .Concat(new[] { int.Parse(numberStringToCheck) })
+                            .ToList();
+            }
+
+            for (int c = numberStringColToCheck - 1; c < numberStringColToCheck + numberStringToCheck.Length + 1; c++)
+            {
+                if (IsAsterisk(numberStringRowToCheck + 1, c))
+                    asteriskAdjacentNumbers[numberStringRowToCheck + 1, c] =
+                        asteriskAdjacentNumbers[numberStringRowToCheck + 1, c]
+                            .Concat(new[] { int.Parse(numberStringToCheck) })
+                            .ToList();
+            }
+
+            if (IsAsterisk(numberStringRowToCheck, numberStringColToCheck - 1))
+                asteriskAdjacentNumbers[numberStringRowToCheck, numberStringColToCheck - 1] =
+                    asteriskAdjacentNumbers[numberStringRowToCheck, numberStringColToCheck - 1]
+                        .Concat(new[] { int.Parse(numberStringToCheck) })
+                        .ToList();
+
+            if (IsAsterisk(numberStringRowToCheck, numberStringColToCheck + numberStringToCheck.Length))
+                asteriskAdjacentNumbers[numberStringRowToCheck, numberStringColToCheck + numberStringToCheck.Length] =
+                    asteriskAdjacentNumbers[numberStringRowToCheck, numberStringColToCheck + numberStringToCheck.Length]
+                        .Concat(new[] { int.Parse(numberStringToCheck) })
+                        .ToList();
+        }
+
+        for (int row = 0; row < h; row++)
+        {
+            string numberString = string.Empty;
+            int numberStringRow = -1;
+            int numberStringCol = -1;
+
+            for (int col = 0; col < w; col++)
+            {
+                if (IsNumber(m[row][col]))
+                {
+                    if (numberString == string.Empty)
+                    {
+                        numberStringCol = col;
+                        numberStringRow = row;
+                    }
+
+                    numberString += m[row][col];
+                }
+                else
+                {
+                    RegisterPossibleGears(numberStringCol, numberStringRow, numberString);
+
+                    numberString = string.Empty;
+                    numberStringRow = -1;
+                    numberStringCol = -1;
+                }
+            }
+
+            RegisterPossibleGears(numberStringCol, numberStringRow, numberString);
+        }
+
+        int sum = 0;
+        for (int i = 0; i < h; i++)
+        {
+            for (int j = 0; j < w; j++)
+            {
+                if (asteriskAdjacentNumbers[i, j].Count == 2)
+                    sum += asteriskAdjacentNumbers[i, j].Aggregate(1, (g1, g2) => g1 * g2);
+            }
+        }
+
+        return sum.ToString();
     }
 }
