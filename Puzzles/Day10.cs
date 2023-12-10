e9aef83708427d766321506f8c84df73afb1ba98
László Zabb <laszlo.zabb@realeyesit.com>
2023-12-10 22:56:02 +0100

    Day10 (2)

--- Puzzles/Day10.cs
+++ Puzzles/Day10.cs
@@ -6,22 +6,21 @@ public class Day10(string input) : DailyPuzzleBase(input)
     {
         var map = Input.Split(Environment.NewLine);
 
-        var start = GetStartCell(map);
+        var start = GetStartLocation(map);
         var startDirection = GetStartDirection(start, map);
 
-
-        var currentCell = start;
-        var nextStepDirection = startDirection;
+        var current = start;
+        var currentDirection = startDirection;
         char currentCellType;
 
         int step = 0;
         do
         {
-            currentCell = Add(currentCell, DirectionToCellDelta(nextStepDirection));
-            currentCellType = map[currentCell.Row][currentCell.Col];
+            current = Add(current, GetLocationDelta(currentDirection));
+            currentCellType = map[current.Row][current.Col];
 
-            if(currentCellType != 'S')
-                nextStepDirection = CellConfigurations[currentCellType][DirectionConnections[nextStepDirection]];
+            if (currentCellType != 'S')
+                currentDirection = CellConfigurations[currentCellType][DirectionToOppositeDirection[currentDirection]];
 
             step++;
         } while (currentCellType != 'S');
@@ -29,21 +28,185 @@ public class Day10(string input) : DailyPuzzleBase(input)
         return (step / 2).ToString();
     }
 
-    private Direction GetStartDirection(Cell start, string[] map)
+    public override string GetPuzzleTwoSolution()
+    {
+        var map = Input.Split(Environment.NewLine);
+
+        var maze = new Cell[map.Length][];
+        var width = map.First().Length;
+        for (int row = 0; row < map.Length; row++)
+        {
+            maze[row] = new Cell[width];
+            for (int col = 0; col < width; col++)
+            {
+                maze[row][col] = new Cell
+                {
+                    Location = new Location(row, col),
+                    Type = map[row][col],
+                    IsPath = false,
+                    IsInside = false
+                };
+            }
+        }
+
+        var start = GetStartLocation(map);
+        var startDirection = GetStartDirection(start, map);
+
+        var startCell = maze[start.Row][start.Col];
+
+        var startPointConnections = new List<Direction>();
+
+        if (IsConnectingToDirection(map, maze, startCell, Direction.Right)) startPointConnections.Add(Direction.Right);
+        if (IsConnectingToDirection(map, maze, startCell, Direction.Left)) startPointConnections.Add(Direction.Left);
+        if (IsConnectingToDirection(map, maze, startCell, Direction.TopUp)) startPointConnections.Add(Direction.TopUp);
+        if (IsConnectingToDirection(map, maze, startCell, Direction.BottomDown)) startPointConnections.Add(Direction.BottomDown);
+
+        var startCellType = CellConfigurations.Single(kv => kv.Value.Keys.All(k => startPointConnections.Contains(k))).Key;
+        startCell.Type = startCellType;
+
+        var currentLocation = start;
+        var currentDirection = startDirection;
+
+        int leftTurnCount = 0;
+        int rightTurnCount = 0;
+        do
+        {
+            var current = maze[currentLocation.Row][currentLocation.Col];
+            current.IsPath = true;
+            current.IsInside = false;
+
+            currentLocation = Add(currentLocation, GetLocationDelta(currentDirection));
+
+            if (CellRotationDirection.ContainsKey(maze[currentLocation.Row][currentLocation.Col].Type))
+            {
+                var turnDirection = CellRotationDirection[maze[currentLocation.Row][currentLocation.Col].Type][DirectionToOppositeDirection[currentDirection]];
+                if (turnDirection == Direction.Left)
+                    leftTurnCount++;
+                else
+                    rightTurnCount++;
+            }
+
+            if (map[currentLocation.Row][currentLocation.Col] != 'S')
+                currentDirection = CellConfigurations[map[currentLocation.Row][currentLocation.Col]][DirectionToOppositeDirection[currentDirection]];
+        } while (map[currentLocation.Row][currentLocation.Col] != 'S');
+
+        //Start direction
+        var startInsideDirection = RotateRight(startDirection);
+        if (leftTurnCount > rightTurnCount)
+            startInsideDirection = RotateLeft(startDirection);
+
+        MarkInside(map, maze, start, startDirection, startInsideDirection);
+
+        return maze.Sum(mazeRow => mazeRow.Count(c => c.IsInside)).ToString();
+    }
+
+    private void MarkInside(string[] map, Cell[][] maze, Location startLocation, Direction startDirection, Direction startInsideDirection)
+    {
+        var currentLocation = startLocation;
+        var currentDirection = startDirection;
+        var currentInsideDirection = startInsideDirection;
+
+        var currentCell = maze[currentLocation.Row][currentLocation.Col];
+        do
+        {
+            SetIsInsides(maze, currentCell, currentInsideDirection);
+
+            currentLocation = Add(currentLocation, GetLocationDelta(currentDirection));
+            currentCell = maze[currentLocation.Row][currentLocation.Col];
+
+            SetIsInsides(maze, currentCell, currentInsideDirection);
+
+            if (CellRotationDirection.ContainsKey(currentCell.Type))
+            {
+                if (CellRotationDirection[currentCell.Type][DirectionToOppositeDirection[currentDirection]] == Direction.Left)
+                    currentInsideDirection = RotateLeft(currentInsideDirection);
+                else
+                    currentInsideDirection = RotateRight(currentInsideDirection);
+            }
+
+            currentDirection = CellConfigurations[currentCell.Type][DirectionToOppositeDirection[currentDirection]];
+
+        } while (map[currentLocation.Row][currentLocation.Col] != 'S');
+    }
+
+    private Direction RotateRight(Direction d)
+    {
+        switch (d)
+        {
+            case Direction.TopUp:
+                return Direction.Right;
+            case Direction.BottomDown:
+                return Direction.Left;
+            case Direction.Left:
+                return Direction.TopUp;
+            case Direction.Right:
+                return Direction.BottomDown;
+            default:
+                throw new ArgumentOutOfRangeException(nameof(d), d, null);
+        }
+    }
+    
+    private Direction RotateLeft(Direction d)
+    {
+        switch (d)
+        {
+            case Direction.TopUp:
+                return Direction.Left;
+            case Direction.BottomDown:
+                return Direction.Right;
+            case Direction.Left:
+                return Direction.BottomDown;
+            case Direction.Right:
+                return Direction.TopUp;
+            default:
+                throw new ArgumentOutOfRangeException(nameof(d), d, null);
+        }
+    }
+
+    private bool IsConnectingToDirection(string[] map, Cell[][] maze, Cell startCell, Direction direction)
+    {
+        bool isConnecting = false;
+
+        var connectingLocation = Add(startCell.Location, GetLocationDelta(direction));
+        if (!IsOutOfBoundary(map, connectingLocation))
+        {
+            isConnecting = CellConfigurations
+                .Where(kv => kv.Value.Keys.Contains(DirectionToOppositeDirection[direction]))
+                .Select(kv => kv.Key)
+                .Contains(maze[connectingLocation.Row][connectingLocation.Col].Type);
+        }
+
+        return isConnecting;
+    }
+
+    private void SetIsInsides(Cell[][] maze, Cell start, Direction direction)
+    {
+        var currentLocation = start.Location;
+        do
+        {
+            currentLocation = Add(currentLocation, GetLocationDelta(direction));
+
+            if (!maze[currentLocation.Row][currentLocation.Col].IsPath)
+                maze[currentLocation.Row][currentLocation.Col].IsInside = true;
+
+        } while (!maze[currentLocation.Row][currentLocation.Col].IsPath);
+    }
+
+    private Direction GetStartDirection(Location start, string[] map)
     {
         var possibleStartDirections = new List<Direction> { Direction.Left, Direction.Right, Direction.BottomDown, Direction.TopUp };
         var startDirection = possibleStartDirections.First();
 
         foreach (var d in possibleStartDirections)
         {
-            var startCell = Add(start, DirectionToCellDelta(d));
+            var startCell = Add(start, GetLocationDelta(d));
             if (!IsOutOfBoundary(map, startCell))
             {
                 var cellType = map[startCell.Row][startCell.Col];
                 if (cellType != '.')
                 {
                     startDirection = d;
-                    if (CellConfigurations[cellType].ContainsKey(DirectionConnections[startDirection]))
+                    if (CellConfigurations[cellType].ContainsKey(DirectionToOppositeDirection[startDirection]))
                         break;
                 }
             }
@@ -52,12 +215,9 @@ public class Day10(string input) : DailyPuzzleBase(input)
         return startDirection;
     }
 
-    private bool IsOutOfBoundary(string[] map, Cell cell)
-    {
-        return cell.Row < 0 || cell.Col < 0 || cell.Row >= map.Length || cell.Col >= map[0].Length;
-    }
+    private bool IsOutOfBoundary(string[] map, Location location) => location.Row < 0 || location.Col < 0 || location.Row >= map.Length || location.Col >= map[0].Length;
 
-    private static Cell GetStartCell(string[] map)
+    private static Location GetStartLocation(string[] map)
     {
         for (int row = 0; row < map.Length; row++)
         {
@@ -65,7 +225,7 @@ public class Day10(string input) : DailyPuzzleBase(input)
             {
                 if (map[row][col] == 'S')
                 {
-                    return new Cell(row, col);
+                    return new Location(row, col);
                 }
             }
         }
@@ -81,7 +241,7 @@ public class Day10(string input) : DailyPuzzleBase(input)
         Right
     }
 
-    private Dictionary<Direction, Direction> DirectionConnections = new()
+    private Dictionary<Direction, Direction> DirectionToOppositeDirection = new()
     {
         { Direction.TopUp ,Direction.BottomDown},
         { Direction.BottomDown , Direction.TopUp},
@@ -89,9 +249,9 @@ public class Day10(string input) : DailyPuzzleBase(input)
         { Direction.Right ,Direction.Left}
     };
 
-    public Cell Add(Cell a, Cell b) => new(a.Row + b.Row, a.Col + b.Col);
+    public Location Add(Location a, Location b) => new(a.Row + b.Row, a.Col + b.Col);
 
-    public class Cell(int row, int col)
+    public class Location(int row, int col)
     {
         public int Row { get; set; } = row;
         public int Col { get; set; } = col;
@@ -99,9 +259,19 @@ public class Day10(string input) : DailyPuzzleBase(input)
         public override string ToString() => $"({Row}, {Col})";
     }
 
+    public class Cell
+    {
+        public Location Location { get; set; }
+        public char Type { get; set; }
+        public bool IsInside { get; set; }
+        public bool IsPath { get; set; }
+
+        public override string ToString() => $"{Type}-{Location} [I:{IsInside}, L:{IsPath}]";
+    }
+
     private Dictionary<char, Dictionary<Direction, Direction>> CellConfigurations = new()
     {
-        //Cell - From - To
+        //Type - From - To
         { '-', new Dictionary<Direction, Direction> { { Direction.Left, Direction.Right}, { Direction.Right, Direction.Left} }},
         { '|', new Dictionary<Direction, Direction> { { Direction.BottomDown, Direction.TopUp}, { Direction.TopUp, Direction.BottomDown} }},
         { 'F', new Dictionary<Direction, Direction> { { Direction.BottomDown, Direction.Right}, { Direction.Right, Direction.BottomDown } }},
@@ -109,26 +279,30 @@ public class Day10(string input) : DailyPuzzleBase(input)
         { '7', new Dictionary<Direction, Direction> { { Direction.Left, Direction.BottomDown}, { Direction.BottomDown, Direction.Left} }},
         { 'J', new Dictionary<Direction, Direction> { { Direction.Left, Direction.TopUp}, { Direction.TopUp, Direction.Left} }},
     };
+    
+    private Dictionary<char, Dictionary<Direction, Direction>> CellRotationDirection = new()
+    {
+        //Type - FromDirection - TurnDirection
+        { 'F', new Dictionary<Direction, Direction> { { Direction.BottomDown, Direction.Right}, { Direction.Right, Direction.Left} }},
+        { 'L', new Dictionary<Direction, Direction> { { Direction.TopUp, Direction.Left}, { Direction.Right, Direction.Right } }},
+        { '7', new Dictionary<Direction, Direction> { { Direction.Left, Direction.Right}, { Direction.BottomDown, Direction.Left} }},
+        { 'J', new Dictionary<Direction, Direction> { { Direction.Left, Direction.Left}, { Direction.TopUp, Direction.Right} }},
+    };
 
-    private Cell DirectionToCellDelta(Direction direction)
+    private Location GetLocationDelta(Direction direction)
     {
         switch (direction)
         {
             case Direction.TopUp:
-                return new Cell(-1, 0);
+                return new Location(-1, 0);
             case Direction.BottomDown:
-                return new Cell(+1, 0);
+                return new Location(+1, 0);
             case Direction.Left:
-                return new Cell(0, -1);
+                return new Location(0, -1);
             case Direction.Right:
-                return new Cell(0, +1);
+                return new Location(0, +1);
             default:
                 throw new ArgumentOutOfRangeException(nameof(direction), direction, null);
         }
-    }
-    
-    public override string GetPuzzleTwoSolution()
-    {
-        return "";
     }
 }
--- Tests/Day10Tests.cs
+++ Tests/Day10Tests.cs
@@ -19,6 +19,59 @@ SJ.L7
 LJ...", "8")]
     public void PuzzleOneTestLong(string input, string output) => Assert.That(GetPuzzle(input).GetPuzzleOneSolution(), Is.EqualTo(output));
 
-    [TestCase(@"", "")]
-    public void PuzzleTwoTest(string input, string output) => Assert.That(GetPuzzle(input).GetPuzzleTwoSolution(), Is.EqualTo(output));
+    [TestCase(@".....
+.S-7.
+.|.|.
+.L-J.
+.....", "1")]
+    public void PuzzleTwoTestSimpleCircle(string input, string output) => Assert.That(GetPuzzle(input).GetPuzzleTwoSolution(), Is.EqualTo(output));
+
+    [TestCase(@"-L|F7
+7S-7|
+L|7||
+-L-J|
+L|-JF", "1")]
+    public void PuzzleTwoTestSimpleCircleWithJunkPipes(string input, string output) => Assert.That(GetPuzzle(input).GetPuzzleTwoSolution(), Is.EqualTo(output));
+
+    [TestCase(@"..F7.
+.FJ|.
+SJ.L7
+|F--J
+LJ...", "1")]
+    public void PuzzleTwoTestSimpleCurves(string input, string output) => Assert.That(GetPuzzle(input).GetPuzzleTwoSolution(), Is.EqualTo(output));
+
+    [TestCase(@"...........
+.S-------7.
+.|F-----7|.
+.||.....||.
+.||.....||.
+.|L-7.F-J|.
+.|..|.|..|.
+.L--J.L--J.
+...........", "4")]
+    public void PuzzleTwoTestSymmetricCurvesWithConnectionToTheBorder(string input, string output) => Assert.That(GetPuzzle(input).GetPuzzleTwoSolution(), Is.EqualTo(output));
+
+    [TestCase(@".F----7F7F7F7F-7....
+.|F--7||||||||FJ....
+.||.FJ||||||||L7....
+FJL7L7LJLJ||LJ.L-7..
+L--J.L7...LJS7F-7L7.
+....F-J..F7FJ|L7L7L7
+....L7.F7||L7|.L7L7|
+.....|FJLJ|FJ|F7|.LJ
+....FJL-7.||.||||...
+....L---J.LJ.LJLJ...", "8")]
+    public void PuzzleTwoTestComplexCase(string input, string output) => Assert.That(GetPuzzle(input).GetPuzzleTwoSolution(), Is.EqualTo(output));
+    
+    [TestCase(@"FF7FSF7F7F7F7F7F---7
+L|LJ||||||||||||F--J
+FL-7LJLJ||||||LJL-77
+F--JF--7||LJLJ7F7FJ-
+L---JF-JLJ.||-FJLJJ7
+|F|F-JF---7F7-L7L|7|
+|FFJF7L7F-JF7|JL---7
+7-L-JL7||F7|L7F-7F7|
+L.L7LFJ|||||FJL7||LJ
+L7JLJL-JLJLJL--JLJ.L", "10")]
+    public void PuzzleTwoTestComplexCaseWithJunkPipes(string input, string output) => Assert.That(GetPuzzle(input).GetPuzzleTwoSolution(), Is.EqualTo(output));
 }
