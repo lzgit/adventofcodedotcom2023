2cf5c72c6d0a39e06bb081a23befc803c0e15458
Vari <zabb.laszlo@hotmail.com>
2023-12-27 10:01:47 +0100

    Day25 (1)

---
+++ Puzzles/Day25.cs
@@ -0,0 +1,86 @@
+﻿namespace Puzzles;
+
+public class Day25(string input) : DailyPuzzleBase(input)
+{
+    public override string GetPuzzleOneSolution()
+    {
+        var wireGraph = GetWireGraph();
+        var originalReachableNodeCount = CountNodes(wireGraph);
+
+        var split = new List<string>();
+        while (split.Count < originalReachableNodeCount)
+        {
+            var maxByExternalConnections = wireGraph.Keys
+                .Where(n => !split.Contains(n))
+                .MaxBy(n => wireGraph[n].Count(nb => split.Contains(nb)));
+
+            split.Add(maxByExternalConnections);
+            var sum = split.Sum(n => wireGraph[n].Count(nb => !split.Contains(nb)));
+            if (sum == 3)
+                return (split.Count * (originalReachableNodeCount - split.Count)).ToString();
+        }
+
+        throw new Exception("!");
+    }
+
+    public override string GetPuzzleTwoSolution()
+    {
+        return "";
+    }
+
+    private static List<(string From, string To)> GetWires(Dictionary<string, List<string>> wireGraph)
+    {
+        return wireGraph
+            .OrderBy(kv => kv.Value.Count)
+            .SelectMany(kv => kv.Value.Select(v => (From: kv.Key, To: v)))
+            .DistinctBy(w => string.Join("-", new List<string> { w.From, w.To }.OrderBy(n => n)))
+            .ToList();
+    }
+
+    private int CountNodes(Dictionary<string, List<string>> graph)
+    {
+        var start = graph.Keys.First();
+
+        var q = new Queue<string>();
+        q.Enqueue(start);
+
+        var isVisited = new HashSet<string>();
+        int count = 0;
+        while (q.TryDequeue(out var nodeName))
+        {
+            if (!isVisited.Contains(nodeName))
+            {
+                isVisited.Add(nodeName);
+                count++;
+                graph[nodeName].Where(n => !isVisited.Contains(n)).ToList().ForEach(q.Enqueue);
+            }
+        }
+
+        return count;
+    }
+
+    private Dictionary<string, List<string>> GetWireGraph()
+    {
+        var graph = new Dictionary<string, List<string>>();
+        foreach (var l in Input.Split(Environment.NewLine))
+        {
+            var graphNodeParts = l.Split(":");
+            var nodeName = graphNodeParts.First().Trim();
+            var neighbours = graphNodeParts.Last().Trim().Split(" ").Select(n => n.Trim()).ToList();
+
+            if (!graph.ContainsKey(nodeName))
+                graph[nodeName] = new List<string>();
+
+            graph[nodeName].AddRange(neighbours);
+            foreach (var neighbour in neighbours)
+            {
+                if (!graph.ContainsKey(neighbour))
+                    graph[neighbour] = new List<string>();
+
+                graph[neighbour].Add(nodeName);
+            }
+        }
+
+        return graph;
+    }
+}
--- Tests/Day25Tests.cs
+++ Tests/Day25Tests.cs
@@ -3,9 +3,21 @@ using Puzzles;
 namespace Tests;
 
 [TestFixture]
-public class Day1XTests : DayTestBase<Day1X>
+public class Day25Tests : DayTestBase<Day25>
 {
-    [TestCase(@"", "")]
+    [TestCase(@"jqt: rhn xhk nvd
+rsh: frs pzl lsr
+xhk: hfx
+cmg: qnr nvd lhk bvb
+rhn: xhk bvb hfx
+bvb: xhk hfx
+pzl: lsr hfx nvd
+qnr: nvd
+ntq: jqt hfx bvb xhk
+nvd: lhk
+lsr: lhk
+rzs: qnr cmg lsr rsh
+frs: qnr lhk lsr", "54")]
     public void PuzzleOneTest(string input, string output) => Assert.That(GetPuzzle(input).GetPuzzleOneSolution(), Is.EqualTo(output));
 
     [TestCase(@"", "")]
